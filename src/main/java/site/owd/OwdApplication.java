78092e8c3c962cca0bd41d764ee09eebd5f229fb
Hwangseoeun <sunny030921@gmail.com>
2025-05-22 21:56:24 +0900

    init project

---
+++ src/main/java/site/owd/OwdApplication.java
@@ -0,0 +1,13 @@
+package site.owd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/owd/OwdApplicationTests.java
@@ -0,0 +1,13 @@
+package site.owd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
