4ff98096bb0fe624ac0308b48ab5971041a3ff08
duol9 <lhyy0195@gmail.com>
2025-05-24 03:25:41 +0900

    chore: remove file

--- src/main/java/site/owd/config/SwaggerConfig.java
+++ src/main/java/site/owd/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -15,6 +16,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+            .addServersItem(new Server().url("/"))
             .info(apiInfo())
             .components(securitySchemeComponents())
             .addSecurityItem(securityRequirement());
