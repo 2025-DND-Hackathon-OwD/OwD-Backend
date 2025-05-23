30443ae07d6faf21fc49afcd2ea8fa1c2341610e
Hwangseoeun <sunny030921@gmail.com>
2025-05-23 10:32:08 +0900

    chore: Swagger 어노테이션 인터페이스 내부로 이동 기존에 controller에 붙어있던 Swagger 문서 관련 어노테이션들을 인터페이스 내부로 이동시킴

--- src/main/java/site/owd/docs/CommonControllerDocs.java
+++ src/main/java/site/owd/docs/CommonControllerDocs.java
@@ -1,4 +1,19 @@
 package site.owd.docs;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Test API")
 public interface CommonControllerDocs {
+
+    @Operation(summary = "test get api")
+    @ApiResponse(responseCode = "404", description = "올바르지 않은 경로입니다.")
+    GetApiDto getApi();
+
+    @Operation(summary = "test post api")
+    String postApi(PostApiDto dto);
+
+    @Operation(summary = "test delete api")
+    String deleteApi(Long memberId);
 }
--- src/main/java/site/owd/docs/DocsTestController.java
+++ src/main/java/site/owd/docs/DocsTestController.java
@@ -1,21 +1,15 @@
 package site.owd.docs;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Test API")
 @RestController
-public class DocsTestController {
+public class DocsTestController implements CommonControllerDocs{
 
-    @Operation(summary = "test get api")
-    @ApiResponse(responseCode = "404", description = "올바르지 않은 경로입니다.")
     @GetMapping("/api/get-api")
     public GetApiDto getApi() {
         final GetApiDto dto = new GetApiDto(
@@ -37,7 +31,7 @@ public class DocsTestController {
 
     @DeleteMapping("/api/delete-api/{memberId}")
     public String deleteApi(
-        @RequestParam Long memberId
+        @PathVariable Long memberId
     ) {
         return "delete api: " + memberId;
     }
