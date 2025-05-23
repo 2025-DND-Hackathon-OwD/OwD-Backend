7cb88e5a54d369bf9b22ec778b8f4f23e2823edb
Hwangseoeun <sunny030921@gmail.com>
2025-05-24 05:21:50 +0900

    fix: jwt 필터 제거

--- src/main/java/site/owd/controller/MemberController.java
+++ src/main/java/site/owd/controller/MemberController.java
@@ -1,32 +1,23 @@
 package site.owd.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import site.owd.dto.LoginDto;
-import site.owd.dto.TokenResponseDto;
-import site.owd.service.TokenService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class MemberController {
 
-    private final TokenService tokenService;
-
-    @PostMapping("/login")
+/*    @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> issueAccessToken(@RequestBody LoginDto loginDto) {
         TokenResponseDto response = tokenService.generateAccessToken(loginDto);
         return ResponseEntity.ok(response);
-    }
-/*
-    @PostMapping("/role/{role}")
-    public ResponseEntity<HttpStatus> saveRole(, @RequestParam("role") String role) {
+    }*/
+
+/*    @PostMapping("/role/{role}")
+    public ResponseEntity<HttpStatus> saveRole(@RequestParam("role") String role) {
 
     }*/
 }
