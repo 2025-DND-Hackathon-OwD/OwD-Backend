1513c5125cf3c4b40a832c5443502a15daf40568
Hwangseoeun <sunny030921@gmail.com>
2025-05-24 05:50:15 +0900

    feat: 가게 상세 조회 api 구현

--- src/main/java/site/owd/controller/PromotionController.java
+++ src/main/java/site/owd/controller/PromotionController.java
@@ -3,11 +3,14 @@ package site.owd.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import site.owd.common.response.ApiResponse;
 import site.owd.dto.CreatePromotionGroupDto;
+import site.owd.dto.PromotionGroupInfoDto;
 import site.owd.service.PromotionService;
+import site.owd.service.StoreService;
 
 import static site.owd.controller.PromotionGroupSuccessCode.*;
 
@@ -17,11 +20,19 @@ import static site.owd.controller.PromotionGroupSuccessCode.*;
 public class PromotionController {
 
     private final PromotionService promotionService;
+    private final StoreService storeService;
 
     @GetMapping("/promotion")
     public ResponseEntity<ApiResponse<CreatePromotionGroupDto>> getRandomPromotions() {
         CreatePromotionGroupDto result = promotionService.createPromotionGroup();
         return ResponseEntity.ok(ApiResponse.success(GET_PROMOTION_GROUPS, result));
+    }
+
+    public ResponseEntity<ApiResponse<PromotionGroupInfoDto>> getStoreInfo(
+        @PathVariable Long storeId
+    ) {
+        PromotionGroupInfoDto result = storeService.getPromotionGroupInfo(storeId);
+        return ResponseEntity.ok(ApiResponse.success(StoreSuccessCode.STORE_FOUND, result));
     }
 
 }
--- src/main/java/site/owd/service/StoreService.java
+++ src/main/java/site/owd/service/StoreService.java
@@ -4,8 +4,11 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.owd.common.exception.CustomException;
+import site.owd.dto.PromotionGroupInfoDto;
 import site.owd.dto.SearchStoreDto;
 import site.owd.entity.Store;
+import site.owd.exception.StoreExceptionCode;
 import site.owd.repository.StoreRepository;
 
 @Service
@@ -21,5 +24,21 @@ public class StoreService {
         return getSearchStore.stream()
             .map(SearchStoreDto::from)
             .collect(Collectors.toList());
+    }
+
+    public PromotionGroupInfoDto getPromotionGroupInfo(Long storeId) {
+        Store store = storeRepository.findById(storeId)
+            .orElseThrow(() -> new CustomException(StoreExceptionCode.STORE_NOT_FOUND));
+
+        double randomDistance = Math.round((Math.random() * 5.0) * 100.0) / 100.0;
+
+        return new PromotionGroupInfoDto(
+            store.getName(),
+            store.getCategory(),
+            store.getAddress(),
+            store.getImage(),
+            store.getPromotionRate(),
+            randomDistance
+        );
     }
 }
