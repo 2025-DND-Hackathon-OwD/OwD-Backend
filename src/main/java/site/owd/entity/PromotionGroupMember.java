fa776dc2f7be55611fee634ab1ec826e30a09e49
duol9 <lhyy0195@gmail.com>
2025-05-24 05:43:07 +0900

    fix: PromotionGroupMember 연관관계 수정

--- src/main/java/site/owd/entity/PromotionGroupMember.java
+++ src/main/java/site/owd/entity/PromotionGroupMember.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -27,7 +28,7 @@ public class PromotionGroupMember {
     private PromotionGroup promotionGroup;
 
     @JoinColumn(name = "store_id")
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Store store;
 
     public PromotionGroupMember(PromotionGroup promotionGroup, Store store) {
